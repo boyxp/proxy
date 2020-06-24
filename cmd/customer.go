4965d49b74e69fe2367559d842e95b7ecd3a80cd
lee <isme@live.com>
2020-06-24 15:43:51 +0800

    批量连接

--- cmd/customer.go
+++ cmd/customer.go
@@ -13,6 +13,7 @@ func main() {
 	//处理传入参数
 	ip   := flag.String("ip", "127.0.0.1", "代理服务器IP地址")
 	port := flag.Int("port", 9999, "代理服务器端口")
+	num  := flag.Int("num", 100, "连接数")
 	flag.Parse()
 
 	if flag.NFlag() < 2 {
@@ -23,12 +24,21 @@ func main() {
 	fmt.Println(*ip)
 	fmt.Println(*port)
 
+	for i:=0;i<*num;i++ {
+		go connect(*ip, *port, i)
+	}
+
+	run := make(chan int)
+	<- run
+}
+
+func connect(ip string, port int, seq int) {
 	//建立连接
-	server := *ip+":"+strconv.Itoa(*port)
+	server := ip+":"+strconv.Itoa(port)
 
 	conn, err := net.Dial("tcp", server)
 	if err != nil {
-		fmt.Println(err)
+		Log(Now(), "连接失败：seq=", seq, "err=", err)
 		return
 	}
 
@@ -40,39 +50,41 @@ func main() {
 	reader := bufio.NewReader(conn)
 
 	ver, _ := reader.ReadByte()
-	Log("版本：", ver)
+	Log(Now(), "版本：seq=", seq, "ver=", ver)
 
 	method, _ := reader.ReadByte()
-	Log("method：", method)
+	Log(Now(), "method：seq=", seq, "method=", method)
 
-	Log("写入模拟请求")
+	Log(Now(), "写入模拟请求：seq=", seq)
 
 	//写入真实请求
-	query(conn)
+	query(conn, seq)
 }
 
-func query(conn net.Conn) {
+func query(conn net.Conn, seq int) {
 	defer close(conn)
 
 	buf := make([]byte, 8192)
 	for i := 0;; i++ {
 		conn.Write([]byte(strconv.Itoa(i)+"query"))
 
+		Log(Now(), "发送：seq=", seq, "msg=", i)
+
 		count, err := conn.Read(buf)
 		if err != nil {
 			if err == io.EOF && count > 0 {
-				Log("服务器报错：", buf[:count])
+				Log(Now(), "服务器报错：seq=", seq, "err=", buf[:count])
 			}
 
 			if err == io.EOF  && count == 0 {
-				Log("设备连接关闭")
+				Log(Now(), "设备连接关闭：seq=", seq)
 			}
 
 			break
 		}
 
 		if count > 0 {
-			Log("接收：", i, string(buf[:count]))
+			Log(Now(), "接收：seq=", seq, "msg=", i, "data=", string(buf[:count]))
 		}
 
 		time.Sleep(1*time.Second)
@@ -87,4 +99,8 @@ func close(conn net.Conn) {
 func Log(v ...interface{}) {
     fmt.Println(v...)
     return
+}
+
+func Now() string {
+	return time.Now().Format("2006-01-02T15:04:05.999999999Z07:00")
 }
