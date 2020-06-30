b6017a66c323afc960864059d34f4f47f52e24dd
lee <isme@live.com>
2020-06-30 11:29:05 +0800

    实时统计

--- proxy/pool.go
+++ proxy/pool.go
@@ -2,47 +2,37 @@ package proxy
 
 import "net"
 import "errors"
-import "sync"
 
 type TcpPool struct {
 	pool chan net.Conn
-	count int
 	max int
-	locker sync.Mutex
 }
 
 func (this *TcpPool) Init(max int) {
 	this.pool  = make(chan net.Conn, max)
-	this.count = 0
 	this.max   = max
 }
 
 func (this *TcpPool) Get() (conn net.Conn, err error) {
-	if this.count < 1 {
+	if len(this.pool) < 1 {
 		err = errors.New("连接池没有可用TCP连接")
 		return
 	}
 
-	this.locker.Lock()
 	conn = <-this.pool
-	this.count--
-	this.locker.Unlock()
 	return
 }
 
 func (this *TcpPool) Put(conn net.Conn) (res bool, err error) {
-	if this.count >= this.max {
+	if len(this.pool) >= this.max {
 		err = errors.New("连接池已满")
 		return
 	}
 
-	this.locker.Lock()
 	this.pool<- conn
-	this.count++
-	this.locker.Unlock()
 	return true, nil
 }
 
 func (this *TcpPool) Len() int {
-	return this.count
+	return len(this.pool)
 }
